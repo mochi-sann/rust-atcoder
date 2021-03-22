b96feb667a462d9c1214d8817acae591cb28a0b6
mochi-sann <moyurusuto@gmail.com>
2021-03-22 11:23:09 +0900

    Readme.mdを修正した

---
+++ abc196/src/bin/f.rs
@@ -0,0 +1,19 @@
+
+use proconio::input;
+
+fn main() {
+    input! {
+        n: i32,
+    }
+    let mut result: f64 = 0.0;
+    for i in 0..n {
+        if x - (a + 0.5) > 0.0 && (result > p || result == 0.0) {
+          // println!("x - ( a + 0.5) = {}", x - ( a + 0.5));
+          // println!("a = {}", a);
+            result = p;
+        }
+    }
+    println!("{}", result);
+}
+
+
